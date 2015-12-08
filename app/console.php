196a3eb8a75ba08e78380f98ab6911e0dbce4af8
Danny Smart <downsider84@hotmail.com>
2015-12-08 22:51:23 +0000

    console and log processing command

---
+++ app/console.php
@@ -0,0 +1,56 @@
+<?php
+
+$appDir = dirname(__DIR__);
+
+include_once($appDir . "/vendor/autoload.php");
+
+use Silktide\Syringe\ReferenceResolver;
+use Silktide\Syringe\ContainerBuilder;
+use Silktide\Syringe\Loader\JsonLoader;
+use Silktide\Syringe\Loader\YamlLoader;
+use Downsider\LoggerheadApp\PuzzleConfig;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+$resolver = new ReferenceResolver();
+$loaders = [
+    new JsonLoader(),
+    new YamlLoader()
+];
+
+$configPaths = [
+    $appDir . "/app/config",
+    $appDir
+];
+
+$builder = new ContainerBuilder($resolver, $configPaths);
+
+foreach ($loaders as $loader) {
+    $builder->addLoader($loader);
+}
+
+$puzzleConfigs = PuzzleConfig::getConfigPaths("silktide/syringe");
+$builder->addConfigFiles($puzzleConfigs);
+$builder->addConfigFile("services.yml");
+
+$container = $builder->createContainer();
+
+// find all commands and add them to the application
+$serviceNames = $container->keys();
+$application = new Application("CLI", "1.0.0");
+foreach ($serviceNames as $name) {
+    // commands are suffixed with ".command" ...
+    if (strrpos($name, ".command") == strlen($name) - 8) {
+        $command = $container[$name];
+        // ... and are instances of the Symfony Command class
+        if ($command instanceof Command) {
+            $application->add($command);
+        }
+    }
+}
+
+// Run the app
+$application->run();
---
+++ src/Command/ProcessLogMessage.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package loggerhead-app
+ * @copyright Copyright © 2015 Danny Smart
+ */
+
+namespace Downsider\LoggerheadApp\Command;
+
+use Downsider\LoggerheadApp\Exception\LogProcessingException;
+use Silktide\QueueBall\Queue\AbstractQueue;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Downsider\LoggerheadApp\Log\MessageProcessor;
+use Downsider\LoggerheadApp\Log\LogRepository;
+
+class ProcessLogMessage extends Command
+{
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var AbstractQueue
+     */
+    protected $queue;
+
+    /**
+     * @var MessageProcessor
+     */
+    protected $procesor;
+
+    /**
+     * @var LogRepository
+     */
+    protected $logRepo;
+
+    public function __construct(AbstractQueue $queue, MessageProcessor $processor, LogRepository $logRepo)
+    {
+        $this->queue = $queue;
+        $this->procesor = $processor;
+        $this->logRepo = $logRepo;
+
+        parent::__construct();
+    }
+
+    public function configure()
+    {
+        $this->setName("downsider:loggerhead:process")
+            ->setDescription("Process log messages from the queue into the database");
+
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+        $this->output->writeln("receiving log message");
+        //$queueMessage = $this->queue->receiveMessage();
+
+        try {
+            //$this->procesor->process($queueMessage->getMessage());
+            $this->procesor->process(json_encode(["format" => "test", "entry" => ["timestamp" => 15263547]]));
+        } catch (LogProcessingException $e) {
+            $this->displayError($e);
+            return;
+        }
+
+        $format = $this->procesor->getFormat();
+        $this->output->writeln("<comment>Using the '{$format->getName()}' format</comment>");
+
+        die("\nformat: " . print_r($format, true));
+
+        $this->logRepo->setFormat($format);
+        $this->logRepo->save($this->procesor->getLog());
+        $this->output->writeln("<info>Log record saved</info>");
+    }
+
+    protected function displayError(LogProcessingException $e)
+    {
+        $this->output->writeln("<error> {$e->getMessage()} </error>");
+        $comment = $e->getComment();
+        if (!empty($comment)) {
+            $this->output->writeln("<comment>$comment</comment>");
+        }
+        $data = $e->getData();
+        if (!empty($data)) {
+            $this->output->writeln($data);
+        }
+    }
+
+} 
