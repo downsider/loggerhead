f1e474fc41e273dc28f19182d9f4a30c5aaf25db
Danny Smart <downsider84@hotmail.com>
2015-12-08 22:50:54 +0000

    extract data from a raw log message, load the format and create a log record

---
+++ src/Exception/LogProcessingException.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package loggerhead-app
+ * @copyright Copyright © 2015 Danny Smart
+ */
+
+namespace Downsider\LoggerheadApp\Exception;
+
+class LogProcessingException extends \Exception
+{
+
+    /**
+     * @var string
+     */
+    protected $comment;
+
+    /**
+     * @var string
+     */
+    protected $data;
+
+    /**
+     * @param string $message
+     * @param string $comment
+     * @param string $data
+     */
+    public function __construct($message, $comment = "", $data = "")
+    {
+        $this->comment = $comment;
+        $this->data = $data;
+        parent::__construct($message);
+    }
+
+    /**
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+}
---
+++ src/Log/MessageProcessor.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package loggerhead-app
+ * @copyright Copyright © 2015 Danny Smart
+ */
+
+namespace Downsider\LoggerheadApp\Log;
+
+use Downsider\LoggerheadApp\Exception\LogProcessingException;
+use Downsider\LoggerheadApp\Format\FormatRepository;
+use Downsider\LoggerheadApp\Format\Format;
+
+class MessageProcessor 
+{
+    /**
+     * @var FormatRepository
+     */
+    protected $formatRepo;
+
+    /**
+     * @var LogFactoryInterface
+     */
+    protected $logFactory;
+
+    /**
+     * @var Format
+     */
+    protected $format;
+
+    /**
+     * @var Log
+     */
+    protected $log;
+
+    public function __construct(FormatRepository $formatRepo, LogFactoryInterface $logFactory)
+    {
+        $this->formatRepo = $formatRepo;
+        $this->logFactory = $logFactory;
+    }
+
+    public function process($messageJson)
+    {
+        // reset state
+        $this->format = null;
+        $this->log = null;
+
+        // decode and validate the message
+        $message = json_decode($messageJson, true);
+        if (empty($message)) {
+            throw new LogProcessingException("The received message was invalid: " . json_last_error_msg(), "Raw message:", $messageJson);
+        }
+
+        if (empty($message["format"]) || empty($message["entry"])) {
+            throw new LogProcessingException("The received message was not in the correct format", "Both 'format' and 'entry' fields are required");
+        }
+
+        // try to load the format from the database
+        $format = $message["format"];
+        $formatList = $this->formatRepo->filter(["name" => $format]);
+        if (empty($formatList[0])) {
+            throw new LogProcessingException("Format '$format' not found");
+        }
+
+        $this->format = $formatList[0];
+        $this->log = $this->logFactory->create($this->format->getName(), $message["entry"]);
+
+    }
+
+    /**
+     * @return Format
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * @return Log
+     */
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+} 
